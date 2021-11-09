1cc96bcc90e470c2b935b582d33acd37b2703391
Hanlong Fu <hfu@salemstate.edu>
2021-11-08 23:13:22 -0800

    Fixed FXML

--- src/controller/MainMenuController.java
+++ src/controller/MainMenuController.java
@@ -1,12 +1,79 @@
 package controller;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
+import java.awt.*;
+import java.io.File;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class MainMenuController implements Initializable {
+    @FXML
+    public void onActionCustomersBtn() {
+        try {
+            Stage stage = new Stage();
+            Parent root = FXMLLoader.load(getClass().getResource("/view/customerMain.fxml"));
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+        } catch (IOException e) {
+            System.out.println("Error in accessing customers window: " + e.getMessage());
+        }
 
+    }
+
+    @FXML
+    public void onActionAppointmentsBtn() {
+        try {
+            Stage stage = new Stage();
+            Parent root = FXMLLoader.load(getClass().getResource("/view/apptMain.fxml"));
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+        } catch (IOException e) {
+            System.out.println("Error in accessing appointments window: " + e.getMessage());
+        }
+
+    }
+
+    @FXML
+    public void onActionReportsBtn() {
+        try {
+            Stage stage = new Stage();
+            Parent root = FXMLLoader.load(getClass().getResource("/view/reports.fxml"));
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+        } catch (IOException e) {
+            System.out.println("Error in accessing reports window: " + e.getMessage());
+        }
+
+    }
+
+    @FXML
+    public void onActionLogsBtn() {
+        File file = new File("log.txt");
+        if(file.exists()) {
+            if(Desktop.isDesktopSupported()) {
+                try {
+                    Desktop.getDesktop().open(file);
+                } catch (IOException e) {
+                    System.out.println("Error opening log file: " + e.getMessage());
+                }
+            }
+        }
+    }
+
+    @FXML
+    public void onActionExitBtn() throws IOException {
+        System.exit(0);
+    }
 
 
     @Override
